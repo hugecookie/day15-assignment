3140c25b95e0163785d145a34ce49f75220acd14
hwarang <phrpp5770@gmail.com>
2023-02-03 11:45:14 +0900

    day15_03

--- day15_01.py
+++ day15_01.py
@@ -1,3 +1,4 @@
+pokemons = ["피카츄", "라이츄", "꼬부기", "어니부기", "거북왕"]
 
 def insert_data(position, pokemon):
     if position < 0 or position > len(pokemons):
@@ -12,13 +13,35 @@ def insert_data(position, pokemon):
 
     pokemons[position] = pokemon  # 지정한 위치에 친구 추가
 
+
+def delete_data(pokemon):
+    if pokemon < 0 or pokemon >= len(pokemons):
+        print("데이터를 삭제할 범위를 벗어났습니다.")
+        return
+
+    kLen = len(pokemons)
+    pokemons[pokemon] = None  # 데이터 삭제
+
+    for i in range(pokemon + 1, kLen):
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
+
+    del (pokemons[kLen - 1])
+
+
 if __name__ == "__main__":
-    pokemons = ["피카츄", "라이츄", "꼬부기", "어니부기", "거북왕"]
     print(pokemons)
-    insert_data(2, '가디')
+    # insert_data(2, '가디')
+    delete_data(1)
     print(pokemons)
-    insert_data(6, '피존투')
-    print(pokemons)
+    # insert_data(6, '피존투')
+    # delete_data(3)
+    # print(pokemons)
+    # a = int(input('입력한 이후 삭제할 인덱스 값을 입력하세요.'))
+    # for i in range(len(pokemons)-1, a, -1):
+    #     delete_data(i)
+    #     print(pokemons)
+
 
 # pokemons = list()  # 빈 배열
 # def add_data(pokemon):
@@ -33,3 +56,5 @@ if __name__ == "__main__":
 # add_data('거북왕')
 #
 # print(pokemons)
+
+

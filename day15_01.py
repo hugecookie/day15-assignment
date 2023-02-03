7c6b455ea6964087562e043b4f94efad695d8e5b
hwarang <phrpp5770@gmail.com>
2023-02-03 13:38:09 +0900

    day15-04

--- day15_01.py
+++ day15_01.py
@@ -1,5 +1,6 @@
 pokemons = ["피카츄", "라이츄", "꼬부기", "어니부기", "거북왕"]
 
+
 def insert_data(position, pokemon):
     if position < 0 or position > len(pokemons):
         print("Out of range!")
@@ -14,26 +15,68 @@ def insert_data(position, pokemon):
     pokemons[position] = pokemon  # 지정한 위치에 친구 추가
 
 
-def delete_data(pokemon):
-    if pokemon < 0 or pokemon >= len(pokemons):
+def delete_data(idx):
+    """
+    선형 리스트의 idx위치에 원소 제거
+    :param idx: Index number
+    :return:
+    """
+    if idx < 0 or idx >= len(pokemons):
         print("데이터를 삭제할 범위를 벗어났습니다.")
         return
 
     kLen = len(pokemons)
-    pokemons[pokemon] = None  # 데이터 삭제
+    pokemons[idx] = None  # 데이터 삭제
 
-    for i in range(pokemon + 1, kLen):
+    for i in range(idx + 1, kLen):
         pokemons[i - 1] = pokemons[i]
         pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
 
     del (pokemons[kLen - 1])
 
 
+def add_data(pokemon):
+    '''
+    선형 리스트의 맨 뒤에 원소 삽입
+    :param pokemon: 삽입할 포켓몬 이름
+    :return:
+    '''
+    pokemons.append(None)
+    pokemons[len(pokemons)-1] = pokemon
+
+
+pokmons = []
+select = -1
+
 if __name__ == "__main__":
-    print(pokemons)
+    while True:
+        select = int(input("선택하세요(1: 추가, 2: 삽입, 3: 삭제, 4: 종료)--> "))
+        if select == 1:
+            data = input("추가할 데이터--> ")
+            add_data(data)
+            print(pokemons)
+        elif select == 2:
+            idx = int(input("삽입할 위치--> "))
+            data = input("추가할 데이터--> ")
+            insert_data(idx, data)
+            print(pokemons)
+        elif select == 3:
+            idx = int(input("삭제할 위치--> "))
+            delete_data(idx)
+            print(pokemons)
+        elif select == 4:
+            print(pokemons)
+            # exit()
+            break
+        else:
+            print("menu에서 고르세요")
+            continue
+
+
+    # print(pokemons)
     # insert_data(2, '가디')
-    delete_data(1)
-    print(pokemons)
+    # delete_data(1)
+    # print(pokemons)
     # insert_data(6, '피존투')
     # delete_data(3)
     # print(pokemons)
@@ -41,6 +84,8 @@ if __name__ == "__main__":
     # for i in range(len(pokemons)-1, a, -1):
     #     delete_data(i)
     #     print(pokemons)
+    # add_data('터컴니')
+    # print(pokemons)
 
 
 # pokemons = list()  # 빈 배열
